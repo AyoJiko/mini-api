9d0dcd060809ad3829fd1f5c1a112c25d79bb680
AyoJiko <ayoubjkhr@gmail.com>
2025-02-03 20:50:52 +0100

    Add cors policy all domaine are autorized

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const postRoutes = require('./routes/postRoutes');
+const cors = require("cors");
+
 require('dotenv').config();
 
 
@@ -11,8 +13,22 @@ const PORT = process.env.port || 3000;
 
 const app = express();
 
+
+// 🔹 Active CORS pour toutes les requêtes
+app.use(cors());
+
+// autoriser seulement un domaine spécifique  https://monfrontend.com
+// const corsOptions = {
+//   origin: "https://monfrontend.com", // 🔹 Seul ce domaine est autorisé
+//   methods: ["GET", "POST"], // 🔹 Autorise seulement GET et POST
+//   allowedHeaders: ["Content-Type", "Authorization"], // 🔹 Autorise seulement ces headers
+// };
+
+app.use(cors(corsOptions));
+
 // Middleware pour parser le corps des requêtes
 app.use(bodyParser.json());
+
 
 // Connexion à MongoDB (assure-toi d’avoir une base de données MongoDB en ligne comme MongoDB Atlas)
 // Connexion avec authentification
